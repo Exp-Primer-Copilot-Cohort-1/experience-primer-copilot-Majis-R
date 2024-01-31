58f31c46cb689fd3115bd04b691a17bb56f7d737
Majis-R <157882306+Majis-R@users.noreply.github.com>
2024-01-31 19:24:35 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,8 @@
+function skillsMember() {
+    var member = document.getElementById("member");
+    var memberSkills = document.getElementById("memberSkills");
+    var memberSkillsList = document.getElementById("memberSkillsList");
+    var memberSkillsListItems = document.getElementsByClassName("memberSkillsListItem");
+    var memberSkillsListItemsArray = Array.from(memberSkillsListItems);
+    var memberSkillsListItemsArrayLength = memberSkillsListItemsArray.length;
+    
